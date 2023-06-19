151af59c7657c8f8b1f8715babe1f0ea3fe0230e
endijulian <endijulian080798@gmail.com>
2023-06-19 11:54:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavbarComponent from "./components/NavbarComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarComponent></NavbarComponent>
+    </div>
+  );
+}
+
+export default App;
