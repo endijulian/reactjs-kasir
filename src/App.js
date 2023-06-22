6b74a2038a0b80d2eda835902cb07b650cde20b5
endijulian <endijulian080798@gmail.com>
2023-06-22 14:37:12 +0700

    list menu add cart

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Hasil, ListCategories, NavbarComponent } from "./components";
+import { Hasil, ListCategories, NavbarComponent, Menus } from "./components";
 import { Col, Container, Row } from "react-bootstrap";
 import React, { Component } from "react";
 import { API_URL } from "./utils/constants";
 import axios from "axios";
+import swal from "sweetalert";
 
 export default class App extends Component {
   constructor(props) {
@@ -10,12 +11,14 @@ export default class App extends Component {
 
     this.state = {
       menus: [],
+      categoriYangDipilih: "Makanan",
+      keranjangs: [],
     };
   }
 
   componentDidMount() {
     axios
-      .get(API_URL + "products")
+      .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
       .then((response) => {
         const menus = response.data;
         this.setState({ menus });
@@ -25,19 +28,73 @@ export default class App extends Component {
       });
   }
 
+  changeCategory = (value) => {
+    this.setState({
+      categoriYangDipilih: value,
+      menus: [],
+    });
+
+    axios
+      .get(API_URL + "products?category.nama=" + value)
+      .then((response) => {
+        const menus = response.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  masukKeranjangs = (value) => {
+    const dataKeranjang = {
+      jumlah: 1,
+      total_harga: value.harga,
+      product: value,
+    };
+
+    axios
+      .post(API_URL + "keranjangs", dataKeranjang)
+      .then((response) => {
+        swal({
+          title: "Success add to troli",
+          text: "Success add to troli " + dataKeranjang.product.nama,
+          icon: "success",
+          button: "Okey",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
-    console.log(this.state.menus);
+    const { menus, categoriYangDipilih } = this.state;
+
     return (
       <div className="App">
         <NavbarComponent></NavbarComponent>
         <div className="mt-4">
           <Container fluid>
             <Row>
-              <ListCategories></ListCategories>
+              <ListCategories
+                changeCategory={this.changeCategory}
+                categoriYangDipilih={categoriYangDipilih}
+              ></ListCategories>
               <Col>
                 <h4>
                   <strong>Daftar Product</strong>
                   <hr></hr>
+
+                  <Row>
+                    {menus &&
+                      menus.map((menu) => (
+                        <Menus
+                          key={menu.id}
+                          menu={menu}
+                          masukKeranjangs={this.masukKeranjangs}
+                        ></Menus>
+                      ))}
+                  </Row>
                 </h4>
               </Col>
               <Hasil></Hasil>
--- src/components/ListCategories.js
+++ src/components/ListCategories.js
@@ -1,14 +1,75 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Col, ListGroup } from "react-bootstrap";
+import axios from "axios";
+import { API_URL } from "../utils/constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensils,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return (
+      <FontAwesomeIcon icon={faUtensils} className="mr-2"></FontAwesomeIcon>
+    );
+  if (nama === "Minuman")
+    return <FontAwesomeIcon icon={faCoffee} className="mr-2"></FontAwesomeIcon>;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="mr-2"></FontAwesomeIcon>;
+
+  return <FontAwesomeIcon icon={faUtensils} className="mr-2"></FontAwesomeIcon>;
+};
 
 export default class ListCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + "categories")
+      .then((response) => {
+        const categories = response.data;
+        this.setState({ categories });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    const { categories } = this.state;
+    const { changeCategory, categoriYangDipilih } = this.props;
+
     return (
       <Col md={2} mt={2}>
         <h4>
           <strong>Daftar Kategori</strong>
           <hr></hr>
         </h4>
+
+        <ListGroup>
+          {categories &&
+            categories.map((category) => (
+              <ListGroup.Item
+                key={category.id}
+                onClick={() => changeCategory(category.nama)}
+                className={
+                  categoriYangDipilih === category.nama && "category-aktif"
+                }
+              >
+                <h5>
+                  <Icon nama={category.nama}></Icon> {category.nama}
+                </h5>
+              </ListGroup.Item>
+            ))}
+        </ListGroup>
       </Col>
     );
   }
